fa0a90b9b4a31e6d1d25553ac772678a2e659ba6
sygns13 <cristian_7_70@hotmail.com>
2021-03-24 08:46:50 -0500

    base del proyect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {InputText} from 'primereact/inputtext';
+import 'primeflex/primeflex.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="p-field">
+        <label htmlFor="fieldId">Label</label>
+        <InputText id="fieldId" type="text"/>
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import PrimeReact from 'primereact/api';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+PrimeReact.ripple = true;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
